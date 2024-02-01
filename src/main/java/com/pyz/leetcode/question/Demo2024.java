95108ea4afaf99f42b47f2c383b6b51eb50753d6
peiyanzhao <peiyanzhao@modian.com>
2024-02-01 15:02:30 +0800

    每日任务

--- src/main/java/com/pyz/leetcode/question/Demo2024.java
+++ src/main/java/com/pyz/leetcode/question/Demo2024.java
@@ -796,10 +796,114 @@ public class Demo2024 {
         return -1;
     }
 
+    public static List<String> fullJustify(String[] words, int maxWidth) {
+
+        List<String> ans = new ArrayList<String>();
+        int right = 0, n = words.length;
+        while (true) {
+            int left = right; // 当前行的第一个单词在 words 的位置
+            int sumLen = 0; // 统计这一行单词长度之和
+            // 循环确定当前行可以放多少单词，注意单词之间应至少有一个空格
+            while (right < n && sumLen + words[right].length() + right - left <= maxWidth) {
+                sumLen += words[right++].length();
+            }
+
+            // 当前行是最后一行：单词左对齐，且单词之间应只有一个空格，在行末填充剩余空格
+            if (right == n) {
+                StringBuffer sb = join(words, left, n, " ");
+                sb.append(blank(maxWidth - sb.length()));
+                ans.add(sb.toString());
+                return ans;
+            }
+
+            int numWords = right - left;
+            int numSpaces = maxWidth - sumLen;
+
+            // 当前行只有一个单词：该单词左对齐，在行末填充剩余空格
+            if (numWords == 1) {
+                StringBuffer sb = new StringBuffer(words[left]);
+                sb.append(blank(numSpaces));
+                ans.add(sb.toString());
+                continue;
+            }
+
+            // 当前行不只一个单词
+            int avgSpaces = numSpaces / (numWords - 1);
+            int extraSpaces = numSpaces % (numWords - 1);
+            StringBuffer sb = new StringBuffer();
+            sb.append(join(words, left, left + extraSpaces + 1, blank(avgSpaces + 1))); // 拼接额外加一个空格的单词
+            sb.append(blank(avgSpaces));
+            sb.append(join(words, left + extraSpaces + 1, right, blank(avgSpaces))); // 拼接其余单词
+            ans.add(sb.toString());
+        }
+    }
+
+    // blank 返回长度为 n 的由空格组成的字符串
+    public static String blank(int n) {
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < n; ++i) {
+            sb.append(' ');
+        }
+        return sb.toString();
+    }
+
+    // join 返回用 sep 拼接 [left, right) 范围内的 words 组成的字符串
+    public static StringBuffer join(String[] words, int left, int right, String sep) {
+        StringBuffer sb = new StringBuffer(words[left]);
+        for (int i = left + 1; i < right; ++i) {
+            sb.append(sep);
+            sb.append(words[i]);
+        }
+        return sb;
+    }
+
+    public static boolean isPalindrome(String s) {
+
+        int first = 0;
+        int last = s.length() - 1;
+        int t = s.length() % 2;
+        while (t == 0 ? first <= last : first < last) {
+            while (!Character.isLetterOrDigit(s.charAt(first)) && last > first) {
+                first++;
+            }
+            while (!Character.isLetterOrDigit(s.charAt(last)) && last > first) {
+                last--;
+            }
+            if (Character.toLowerCase(s.charAt(first)) != Character.toLowerCase(s.charAt(last))) {
+                return false;
+            }
+            first++;
+            last--;
+        }
+
+        return true;
+
+    }
+
+    public static boolean isSubsequence(String s, String t) {
+
+        int slen = s.length();
+        int tlen = t.length();
+        int p = 0;
+        for (int i = 0; i < slen; i++) {
+            while (p <= tlen) {
+                if (p == tlen){
+                    return false;
+                }
+                if (t.charAt(p) == s.charAt(i)){
+                    p++;
+                    break;
+                }
+                p++;
+            }
+        }
+
+        return true;
+    }
 
     public static void main(String[] args) {
 
-        System.out.println(strStr("mississippi", "issipi"));
+        System.out.println(isSubsequence("axc","ahbgdc"));
 
     }
 
